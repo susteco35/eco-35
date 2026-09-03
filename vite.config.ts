6c525e76cbcd3edd45a3045f39485671cf0b17a5
Economics <susteco35@gmail.com>
2026-09-03 20:37:40 +0600

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/sust-eco-35/',
+  base: '/eco-35/',
   plugins: [react()],
   resolve: {
     alias: {
