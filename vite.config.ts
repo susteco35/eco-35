b13ed4704ffffddc935cd221e5e515307b4326f0
Economics <susteco35@gmail.com>
2026-09-03 22:34:42 +0600

    Fix formatting in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
-  base: '/eco-35/',
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
+  base: '/eco-35/',
 });
